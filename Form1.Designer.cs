ceb172ef9571dcb9aec60a77325e606415492d32
owl0501 <owl010588@gmail.com>
2021-01-08 14:17:09 +0800

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace RS232_Test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnRecieved = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnConnected = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnDisconnected = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(249, 77);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 37);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "發送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnRecieved
+            // 
+            this.btnRecieved.Location = new System.Drawing.Point(113, 357);
+            this.btnRecieved.Name = "btnRecieved";
+            this.btnRecieved.Size = new System.Drawing.Size(75, 39);
+            this.btnRecieved.TabIndex = 3;
+            this.btnRecieved.Text = "接收";
+            this.btnRecieved.UseVisualStyleBackColor = true;
+            this.btnRecieved.Click += new System.EventHandler(this.btnRecieved_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(41, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 34);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnConnected
+            // 
+            this.btnConnected.Location = new System.Drawing.Point(249, 18);
+            this.btnConnected.Name = "btnConnected";
+            this.btnConnected.Size = new System.Drawing.Size(75, 37);
+            this.btnConnected.TabIndex = 5;
+            this.btnConnected.Text = "連線";
+            this.btnConnected.UseVisualStyleBackColor = true;
+            this.btnConnected.Click += new System.EventHandler(this.btnConnected_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(41, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 34);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.richTextBox1.Location = new System.Drawing.Point(31, 168);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 164);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // btnDisconnected
+            // 
+            this.btnDisconnected.Location = new System.Drawing.Point(218, 357);
+            this.btnDisconnected.Name = "btnDisconnected";
+            this.btnDisconnected.Size = new System.Drawing.Size(83, 39);
+            this.btnDisconnected.TabIndex = 8;
+            this.btnDisconnected.Text = "中斷連線";
+            this.btnDisconnected.UseVisualStyleBackColor = true;
+            this.btnDisconnected.Click += new System.EventHandler(this.btnDisconnected_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 408);
+            this.Controls.Add(this.btnDisconnected);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnConnected);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnRecieved);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnRecieved;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnConnected;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnDisconnected;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace RS232_Test1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //序列埠物件
+        SerialPort sport1 = new SerialPort();
+        //傳送檢查
+        private bool isSending =false;
+        //
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            isSending = false;
+            GetPorts();
+            
+        }
+        //委派事件
+        delegate void dlgMession(string st);
+        private void displayText(string st)
+        {
+            richTextBox1.AppendText(st+"\r\n");
+        }
+        //取得序列埠
+        private void GetPorts()
+        {
+            string[] portnames = SerialPort.GetPortNames();
+            foreach (var item in portnames)
+            {
+                comboBox1.Items.Add(item);
+            }
+        }
+        //連線
+        private void Connected()
+        {
+            try
+            {
+                //鮑率
+                sport1.BaudRate = 9600;
+                //資料位元
+                sport1.DataBits = 8;
+                sport1.PortName = comboBox1.Text;
+                //兩個停止位
+                sport1.StopBits = System.IO.Ports.StopBits.One;
+                //無奇偶校驗位
+                sport1.Parity = System.IO.Ports.Parity.None;
+                sport1.ReadTimeout = 100;
+                //開啟SerialPort
+                sport1.Open();
+                if (!sport1.IsOpen)
+                {
+                    MessageBox.Show("連線失敗");
+                    return;
+                }
+                else
+                {
+                    richTextBox1.AppendText(comboBox1.Text + "連線成功\r\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                //釋放(關閉)SerialPort
+                sport1.Dispose();
+                richTextBox1.AppendText(ex.Message);
+            }
+        }
+        //中斷連線
+        private void Disconnected()
+        {
+            try
+            {
+                if (sport1.IsOpen)
+                {
+                    sport1.Close();
+                    richTextBox1.AppendText($"{sport1.PortName}已中斷連線...\r\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.AppendText(ex.Message);
+            }
+        }
+        //資料傳送
+        private void Sending()
+        {
+            isSending = true;
+            try 
+            {
+                //sport1.Write(textBox2.Text);
+                sport1.Write(textBox2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+            finally
+            {
+                isSending = false;
+            }
+        }
+        //資料接收
+        private void Received()
+        {
+            try
+            {
+                int len = sport1.BytesToRead;
+                string receivedData = string.Empty;
+                if (len != 0)
+                {
+                    byte[] buff = new byte[len];
+                    sport1.Read(buff, 0, len);
+                    receivedData = Encoding.Default.GetString(buff);
+                }
+                //MessageBox.Show($"{receivedData}");
+                //richTextBox1.AppendText(receivedData + "\r\n");
+                dlgMession dlg = new dlgMession(displayText);
+                this.Invoke(dlg, receivedData);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+
+
+        }
+        #region--按鈕事件--
+        private void btnConnected_Click(object sender, EventArgs e)
+        {
+            Connected();
+        }
+        //
+        private void btnDisconnected_Click(object sender, EventArgs e)
+        {
+            Disconnected();
+        }
+        //
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (!isSending)
+            {
+                Thread t = new Thread(Sending);
+                t.IsBackground = true;
+                t.Start();
+            }
+        }
+        //
+        private void btnRecieved_Click(object sender, EventArgs e)
+        {
+            Received();
+        }
+
+        #endregion
+    }
+}
